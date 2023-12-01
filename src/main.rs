21701058c81bd79b51056e25b3de9102bea8408a
Sergey Kozub <sergeykozub@google.com>
2023-12-01 09:36:11 +0100

    template

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    assert!(args.len() > 1, "Task number is missing");
+
+    let number: usize = args[1].parse().unwrap();
+    let filename: String = if args.len() > 2 {
+        args[2].clone()
+    } else {
+        format!("input/day{:02}.txt", number)
+    };
+    let content = fs::read_to_string(filename).expect("Error reading input");
+
+    let days: Vec<fn(&str)> = vec![];
+    days[number - 1](&content)
+}
