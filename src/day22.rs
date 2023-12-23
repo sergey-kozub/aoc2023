62d9ad340d58cf7d7d46451310b97edbbfe1a369
Sergey Kozub <sergey.kozub.ch@gmail.com>
2023-12-23 13:19:18 +0100

    day22 (large)

--- src/day22.rs
+++ src/day22.rs
@@ -4,10 +4,11 @@ use std::ops::RangeInclusive;
 
 type Point = (u32, u32, u32);
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 struct Brick {
   cubes: Vec<Point>,
   vertical: bool,
+  ordinal: usize,
 }
 
 #[derive(Clone)]
@@ -17,7 +18,7 @@ struct Field {
 }
 
 impl Brick {
-  fn parse(text: &str) -> Brick {
+  fn parse(text: &str, ordinal: usize) -> Brick {
     let (s1, s2) = text.split_once('~').unwrap();
     let get = |s: &str| -> Point {
       let a = s.split(',').map(|x| x.parse::<u32>().unwrap())
@@ -29,7 +30,7 @@ impl Brick {
       (a.1..=b.1).flat_map(move |y|
         (a.2..=b.2).map(move |z| (x, y, z))))
         .collect::<Vec<_>>();
-    Brick { cubes, vertical: a.2 != b.2 }
+    Brick { cubes, vertical: a.2 != b.2, ordinal }
   }
 
   fn fall(&self, filled: &HashSet<Point>) -> Option<Brick> {
@@ -44,6 +45,7 @@ impl Brick {
       Brick {
         cubes: self.cubes.iter().map(|&x| sub(x, n)).collect::<Vec<_>>(),
         vertical: self.vertical,
+        ordinal: self.ordinal,
       }
     })
   }
@@ -51,7 +53,8 @@ impl Brick {
 
 impl Field {
   fn parse(text: &str) -> Field {
-    let bricks = text.lines().map(Brick::parse).collect::<Vec<_>>();
+    let bricks = text.lines().enumerate().map(|(k, v)| Brick::parse(v, k))
+      .collect::<Vec<_>>();
     let filled = bricks.iter().flat_map(|x| x.cubes.iter().copied())
       .collect::<HashSet<_>>();
     Field { bricks, filled }
@@ -93,6 +96,17 @@ impl Field {
     }).count()
   }
 
+  fn count_fall(&self, index: usize) -> usize {
+    let mut copy = self.clone();
+    copy.remove(index);
+    copy.settle();
+    copy.bricks.iter().filter(|&a| {
+      let idx = self.bricks.iter()
+        .position(|b| a.ordinal == b.ordinal).unwrap();
+      self.bricks[idx] != *a
+    }).count()
+  }
+
   fn range_x(&self) -> RangeInclusive<u32> {
     let iter_x = || self.filled.iter().map(|p| p.0);
     iter_x().min().unwrap()..=iter_x().max().unwrap()
@@ -109,8 +123,9 @@ impl Field {
   fn view_x(&self) -> String {
     (1..=*self.range_z().end()).rev().fold(String::new(), |s, z| {
       s + &String::from_iter(self.range_x().map(|x| {
-        as_char(self.bricks.iter().enumerate().filter_map(|(k, v)| {
-          if v.cubes.iter().any(|p| p.0 == x && p.2 == z) {Some(k)} else {None}
+        as_char(self.bricks.iter().filter_map(|v| {
+          if v.cubes.iter().any(|p| p.0 == x && p.2 == z)
+            {Some(v.ordinal)} else {None}
         }).collect::<Vec<_>>())
       })) + "\n"
     })
@@ -118,8 +133,9 @@ impl Field {
   fn view_y(&self) -> String {
     (1..=*self.range_z().end()).rev().fold(String::new(), |s, z| {
       s + &String::from_iter(self.range_y().map(|y| {
-        as_char(self.bricks.iter().enumerate().filter_map(|(k, v)| {
-          if v.cubes.iter().any(|p| p.1 == y && p.2 == z) {Some(k)} else {None}
+        as_char(self.bricks.iter().filter_map(|v| {
+          if v.cubes.iter().any(|p| p.1 == y && p.2 == z)
+            {Some(v.ordinal)} else {None}
         }).collect::<Vec<_>>())
       })) + "\n"
     })
@@ -140,7 +156,10 @@ impl fmt::Debug for Field {
 pub fn run(content: &str) {
   let mut field = Field::parse(content);
   field.settle();
-  println!("{}", field.count_safe());
+  let res1 = field.count_safe();
+  let res2 = (0..field.bricks.len())
+    .map(|i| field.count_fall(i)).sum::<usize>();
+  println!("{} {}", res1, res2);
 }
 
 #[cfg(test)]
@@ -159,5 +178,14 @@ mod tests {
     let mut test = super::Field::parse(TEST);
     test.settle();
     assert_eq!(test.count_safe(), 5);
+  }
+
+  #[test]
+  fn large() {
+    let mut test = super::Field::parse(TEST);
+    test.settle();
+    let fall = (0..7).map(|i| test.count_fall(i))
+      .filter(|&n| n > 0).collect::<Vec<_>>();
+    assert_eq!(fall, [1, 6]);
   }
 }
